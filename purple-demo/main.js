5963510f4c35cc8891eb95e759555a1349e7994b
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-07-28 09:23:56 +0200

    purple-demo2 (#11)

--- purple-demo/main.js
+++ purple-demo/main.js
@@ -15,15 +15,23 @@ const timer = setInterval(() => {
 
 function conditionallySwitchStage(){
     const contentFrame = document.getElementById('content');
+    let audio = new Audio('../common/sfx/dig.mp3');
 
     if (secondsElapsed == 90){
         contentFrame.src = 'anims/index.html';
+
+        audio = new Audio('../common/sfx/dig.mp3');
+        audio.play();
     }
     else if (secondsElapsed == 90+ 45){
         contentFrame.src = '3scenes3d/index.html';
+
+        audio = new Audio('../common/sfx/NeverGonnaGive.mp3');
+        audio.play();
     }
     else if (secondsElapsed == 90+ 45 + 30){
         contentFrame.src = 'fractals/index.html';
+        audio = new Audio('../common/sfx/Lambada.mp3');
+        audio.play();
     }
-
 }
