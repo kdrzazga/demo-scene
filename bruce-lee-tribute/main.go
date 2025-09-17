c32d2cb3908d6645a1331dd9dff207f64a146ecd
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-09-17 18:01:09 +0200

    Bruce Lee Tribute 1

---
+++ bruce-lee-tribute/main.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+    "runtime"
+	"bytes"
+	"image"
+	_ "image/jpeg"
+	"time"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+    "github.com/hajimehoshi/ebiten/v2/audio/wav"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+)
+
+var (
+    err                         error
+	background          *ebiten.Image
+	background2         *ebiten.Image
+	pic1nun             *ebiten.Image
+	pic2nun             *ebiten.Image
+	pic3nun             *ebiten.Image
+	pic4                *ebiten.Image
+	projectileImg       *ebiten.Image
+	orangeFlyingKickImg *ebiten.Image
+	orangeFlyingKickGif *GIFAnimator
+	mplusFaceSource     *text.GoTextFaceSource
+    context         *audio.Context
+    player          *audio.Player
+    player2         *audio.Player
+    themePlayer     *audio.Player
+    counter         float64
+
+    shiftX       int
+    shiftY       int
+    currentStage int
+
+    startTime time.Time
+)
+
+const (
+    stage1Timeout = 5000
+    stage2Timeout = 1900 + stage1Timeout
+    stage3Timeout = 24000 + stage2Timeout
+    stage4Timeout = 53000 + stage3Timeout
+    stageOutroTimeout = stage4Timeout + 9000
+
+    final = stageOutroTimeout + 100
+
+    stage2MusicPath = "audio/Boards dont hit back.wav"
+    stage3MusicPath = "audio/BruceLee.wav"
+    stage4MusicPath = "audio/Enter The Dragon - Full.wav"
+
+    moveSpeed = 2
+)
+
+func init() {
+    ebiten.SetCursorMode(ebiten.CursorModeHidden)
+
+    shiftX = 0
+    shiftY = 0
+
+    context = audio.NewContext(44100)
+
+    orangeFlyingKickImg, err = loadImage("pics/orange-flying-kick.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    orangeFlyingKickGif, err = NewGIFAnimator("pics/orange-flying-kick.gif", false)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    pic1nun, err = loadImage("pics/bruce-lee-nunchako1.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    pic2nun, err = loadImage("pics/bruce-lee-nunchako2.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    pic3nun, err = loadImage("pics/bruce-lee2.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    pic4, err = loadImage("pics/bl.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    projectileImg, err = loadImage("pics/projectile.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf))
+	if err != nil {
+		log.Fatal(err)
+	}
+	mplusFaceSource = s
+
+	initStage3()
+	initStage4()
+	initStageOutro()
+}
+
+func initAudio(path string) (*audio.Player, error) {
+    f, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    stream, err := wav.Decode(context, f)
+    if err != nil {
+        return nil, err
+    }
+
+    localPlayer, err := audio.NewPlayer(context, stream)
+    if err != nil {
+        return nil, err
+    }
+
+    //defer f.Close()
+    return localPlayer, nil
+}
+
+type Game struct {
+	count int
+}
+
+func (g *Game) Update() error {
+	g.count++
+
+	if (counter > final) {
+        elapsed := time.Since(startTime)
+        log.Printf("Execution time: %v\n", elapsed)
+        return ebiten.Termination
+    }
+
+	if runtime.GOOS == "js" {
+		if ebiten.IsKeyPressed(ebiten.KeyF) || len(inpututil.AppendJustPressedTouchIDs(nil)) > 0 {
+			ebiten.SetFullscreen(true)
+		}
+	}
+    playbackDone := player == nil || !player.IsPlaying()
+
+	if runtime.GOOS != "js" && (ebiten.IsKeyPressed(ebiten.KeyQ) || playbackDone) {
+	    //fmt.Print(counter)
+		return nil
+	}
+
+    if (counter - stage1Timeout <1.0 || counter - stage2Timeout <1.0 || counter - stage3Timeout <1.0 || counter - stage4Timeout <1.0){
+        elapsed := time.Since(startTime)
+        log.Printf("Execution time: %v\n", elapsed)
+    }
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+    counter += 7
+    if (counter < stage1Timeout) {
+        stage1(screen, counter)
+    }else if (counter < stage2Timeout) {
+        stage2(screen)
+    } else if (counter < stage3Timeout) {
+        stage3(screen, counter)
+    } else if (counter < stage4Timeout) {
+        stage4(screen, counter)
+    }else  {
+        stageOutro(screen)
+    }
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	s := ebiten.Monitor().DeviceScaleFactor()
+	return int(float64(outsideWidth) * s), int(float64(outsideHeight) * s)
+}
+
+func main() {
+    startTime = time.Now()
+
+    analyzeArguments()
+
+    ebiten.SetFullscreen(true)
+	ebiten.SetWindowTitle("BRUCE LEE TRIBUTE")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func analyzeArguments(){
+    name := flag.String("stage", "1", "a number of stage to start with")
+    flag.Parse()
+    stage := string(*name)
+    switch(stage){
+        case "2":
+            counter = stage1Timeout
+        case "3":
+            counter = stage2Timeout
+        case "4":
+            counter = stage3Timeout
+        case "4.5":
+            counter = 30000 + stage3Timeout
+        case "outro":
+            counter = stage4Timeout
+    }
+}
+
+func drawBackgroundScaled(screen, bg *ebiten.Image, x, y, w, h int, scale float64) {
+    subImg := bg.SubImage(image.Rect(x, y, w, h)).(*ebiten.Image)
+    op := &ebiten.DrawImageOptions{}
+    op.GeoM.Scale(scale, scale)
+    screen.DrawImage(subImg, op)
+}
+
+func drawBackground(screen, bg *ebiten.Image, x, y, w, h int) {
+    drawBackgroundScaled(screen, bg, x, y, w, h, 2.0)
+}
+
+func loadImage(path string) (*ebiten.Image, error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+    img, _, err := image.Decode(file)
+    if err != nil {
+        return nil, err
+    }
+    return ebiten.NewImageFromImage(img), nil
+}
---
+++ bruce-lee-tribute/stage4.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+    "log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+var (
+    returnOfFuryAnimator *GIFAnimator
+    chuckNorrisAnimator *GIFAnimator
+    kickdownAnimator *GIFAnimator
+    kickingAnimator *GIFAnimator
+    c64gameAnimator *GIFAnimator
+    returnOfFuryImg *ebiten.Image
+	bigPic          *ebiten.Image
+	c64Pic          *ebiten.Image
+	flyingKickPic   *ebiten.Image
+	bleePic         *ebiten.Image
+	bigPicY             int
+	shiftX4             int
+	extraDelay          int
+	bruceleePosition    int
+)
+
+func initStage4(){
+    var err error
+
+    returnOfFuryAnimator, err = NewGIFAnimator("pics/return-of-fury.gif", false)
+    if err != nil {
+        log.Fatal(err)
+    }
+    chuckNorrisAnimator, err = NewGIFAnimator("pics/brucelee-chucknorris.gif", false)
+    if err != nil {
+        log.Fatal(err)
+    }
+    kickdownAnimator, err = NewGIFAnimator("pics/kickdown.gif", false)
+    if err != nil {
+        log.Fatal(err)
+    }
+    returnOfFuryImg, err = loadImage("pics/return-of-fury.jpg")
+    if err != nil {
+        log.Fatal(err)
+    }
+    kickingAnimator, err = NewGIFAnimator("pics/kicking.gif", false)
+    if err != nil {
+        log.Fatal(err)
+    }
+    bigPic, err = loadImage("pics/big3.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    c64Pic, err = loadImage("pics/c64.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    flyingKickPic, err = loadImage("pics/fk.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    bleePic, err = loadImage("pics/blee.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    c64gameAnimator, err = NewGIFAnimator("pics/c64game.gif", false)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    extraDelay = 0
+    bigPicY = 2500
+    shiftX4 = 0
+    bruceleePosition = -4500
+}
+
+func stage4(screen *ebiten.Image, counter float64){
+    extraDelay += 1
+    //log.Println(extraDelay, (extraDelay % 3), (extraDelay %3 == 0), bigPicY)
+    if (extraDelay % 4 > 0 && bigPicY > 0){
+        bigPicY -= 1
+    }
+    if (counter > (30000 + stage3Timeout)){
+        bruceleePosition += 2
+
+        drawRealBruceLee(screen, float64(bruceleePosition))
+        drawAnimator(screen, 606, 224)
+        if (bruceleePosition > 550 - 60 && bruceleePosition < 1030 - 60){
+             drawC64BruceLee(screen, float64(bruceleePosition+30))
+        }
+        drawC64(screen)
+
+    } else if (counter > (25000 + stage3Timeout)){
+        bigPicY += 2
+    } else if (counter > 15000 + stage3Timeout){
+        shiftX4 -= 1
+    }
+
+    if (counter < (30000 + stage3Timeout)){
+        drawBackground(screen, bigPic, shiftX4, bigPicY, 940,811)
+    }
+
+    if (counter < 2000 + stage3Timeout) {
+        returnOfFuryAnimator.Update()
+        returnOfFuryAnimator.Draw(screen, float64(shiftX4), 0)
+    } else if ((counter < 3000 + stage3Timeout) || (counter > 12000 + stage3Timeout && counter < 13000 + stage3Timeout)) {
+        drawBackgroundScaled(screen, returnOfFuryImg, 0, 0, 400+shiftX4, 245, float64(1))
+        chuckNorrisAnimator.Update()
+        chuckNorrisAnimator.Draw(screen, 400+float64(shiftX4), 245)
+    } else if (counter < 8300 + stage3Timeout){
+        drawBackgroundScaled(screen, returnOfFuryImg, 0, 0, 400+shiftX4, 245, float64(1))
+        chuckNorrisAnimator.Draw(screen, 400+float64(shiftX4), 245)
+        kickingAnimator.Update()
+        kickingAnimator.Draw(screen, 10+float64(shiftX4), 245+280)
+    } else if (counter < 30000 + stage3Timeout){
+        drawBackgroundScaled(screen, returnOfFuryImg, shiftX4, 0, 400, 245, float64(1))
+        chuckNorrisAnimator.Draw(screen, 400+float64(shiftX4), 245)
+
+        kickingAnimator.Draw(screen, 10+float64(shiftX4), 245+280)
+
+        kickdownAnimator.Draw(screen, 400+468+float64(shiftX4), 245+280)
+        kickdownAnimator.Update()
+        chuckNorrisAnimator.Reset()
+    }
+
+    if themePlayer == nil{
+        themePlayer, err = initAudio(stage4MusicPath)
+        themePlayer.Play()
+
+        if err != nil {
+        	log.Fatal(err)
+        }
+    }
+}
+
+func drawRealBruceLee(screen *ebiten.Image, position float64) {
+    op := &ebiten.DrawImageOptions{}
+    op.GeoM.Reset()
+    translation := float64(0)
+    if (position > 900){
+        translation = 193
+    }
+
+    if (position < 850-193 || position > 930){
+        op.GeoM.Translate(position - translation, 350)
+        screen.DrawImage(flyingKickPic, op)
+    }
+}
+
+func drawC64BruceLee(screen *ebiten.Image, position float64) {
+    op := &ebiten.DrawImageOptions{}
+    op.GeoM.Reset()
+    op.GeoM.Translate(2*position + 20, 2*432) // 350 + 30
+    op.GeoM.Scale(0.5, 0.5)
+    screen.DrawImage(bleePic, op)
+}
+
+func drawAnimator(screen *ebiten.Image, x, y float64) {
+    c64gameAnimator.Draw(screen, x, y)
+    c64gameAnimator.Update()
+}
+
+func drawC64(screen *ebiten.Image) {
+    drawBackgroundScaled(screen, c64Pic, 0, 0, 1200, 722, 1)
+}
+
