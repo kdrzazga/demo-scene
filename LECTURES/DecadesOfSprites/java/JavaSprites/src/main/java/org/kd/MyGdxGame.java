e053342000983b892fc491b0dd099714c48a72bc
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-10-17 10:50:17 +0200

    java sprites drawing

--- LECTURES/DecadesOfSprites/java/JavaSprites/src/main/java/org/kd/MyGdxGame.java
+++ LECTURES/DecadesOfSprites/java/JavaSprites/src/main/java/org/kd/MyGdxGame.java
@@ -7,35 +7,49 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
     Texture texture;
-    Sprite sprite;
+    List<Sprite> sprites;
 
     @Override
-    public void create () {
+    public void create() {
+        this.sprites = new ArrayList<>(3);
         batch = new SpriteBatch();
         // Load the texture from a file in the assets folder
-        texture = new Texture(Gdx.files.internal("skull.bmp"));
-        // Create a sprite from the texture
-        sprite = new Sprite(texture);
-        // Optionally set position, scale, rotation
-        sprite.setPosition(100, 100);
-        sprite.setScale(2.0f);
+        var positions = Arrays.asList(50, 50, 250, 50, 250, 250);
+        AtomicInteger index = new AtomicInteger(0);
+        Arrays.asList("skull.bmp", "monster.bmp", "tank.bmp").forEach(file -> {
+            texture = new Texture(Gdx.files.internal(file));
+            // Create a sprite from the texture
+            var sprite = new Sprite(texture);
+            // Optionally set position, scale, rotation
+            sprite.setPosition(positions.get(index.get()), positions.get(index.get() +1));
+            index.addAndGet(2);
+            sprite.setScale(2.0f);
+            sprites.add(sprite);
+        });
     }
 
     @Override
-    public void render () {
+    public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        sprite.draw(batch); // draw the sprite
+        sprites.forEach(s -> s.draw(batch));
+        var skull = sprites.get(0);
+        skull.setPosition(skull.getX()+1, skull.getY());
         batch.end();
     }
 
     @Override
-    public void dispose () {
+    public void dispose() {
         batch.dispose();
         texture.dispose();
     }
