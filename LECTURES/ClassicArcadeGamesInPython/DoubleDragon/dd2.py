e736e8e80d82b819e63b8a1d54234e6ad14bb4f0
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-10-23 18:05:03 +0200

    Double dragon level1 Billy inside garage

--- LECTURES/ClassicArcadeGamesInPython/DoubleDragon/dd2.py
+++ LECTURES/ClassicArcadeGamesInPython/DoubleDragon/dd2.py
@@ -10,13 +10,20 @@ SCREEN_TITLE = "Double Python"
 class DoubleDragonPython(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-        self.background =  background.Background.from_file("resources/Mission1.png")
 
+        self.counter = 0
+        self.background =  background.Background.from_file("resources/Mission1.png")
         self.garage_door_y = 175
+
         door = arcade.Sprite()
         door.texture = arcade.load_texture("resources/door.png")
         door.center_x = 109
         door.center_y = self.garage_door_y
+
+        billy = arcade.Sprite()
+        billy.texture = arcade.load_texture("resources/billy1.png")
+        billy.center_x = 109
+        billy.center_y = self.garage_door_y - 15
 
         over_garage = arcade.Sprite()
         over_garage.texture = arcade.load_texture("resources/overGarage.png")
@@ -24,16 +31,23 @@ class DoubleDragonPython(arcade.Window):
         over_garage.center_y = 320
 
         self.bg_sprite_list = arcade.SpriteList()
+        self.bg_sprite_list.append(billy)
         self.bg_sprite_list.append(door)
         self.bg_sprite_list.append(over_garage)
 
     def on_update(self, delta_time):
-        print("update ")
+        self.counter += 1
+        print("update ", self.counter)
+        self.conditionally_move_garage_door()
 
     def on_draw(self):
         print("draw")
         self.background.draw()
         self.bg_sprite_list.draw()
+
+    def conditionally_move_garage_door(self):
+        if self.counter > 300 and self.counter < 800:
+            self.bg_sprite_list[1].center_y += 0.3
 
 def main():
     game = DoubleDragonPython()
