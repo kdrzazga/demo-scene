50aa99ddad562e0a5874ffc3cb0c124f59228241
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-11-08 22:37:23 +0100

    FACES petscii 6 - split to  3 files

--- pics/non-contest/faces/sprites.asm
+++ pics/non-contest/faces/sprites.asm
@@ -1,67 +1,102 @@
-//used by 3sprites
-init_sprites:
-	ldx #$00
-petla:	lda sprite_colors, x
-	sta $d027, x
-	inx
-	cpx #$8
-	bne petla
-	
-    // set sprite pointer index
-    // this, multiplied by $40, is the address
-    // in this case, the address is $2000
-    // $80 * $40 = $2000
-	//sprite0:
-    lda #$80
-    sta $07f8
+    init_sprites:
+    
+	// set sprite multicolors
+	lda #YELLOW
+	sta $d025
+	lda #BLACK
+	sta $d026
 
-	//sprite1:
-    // $81 * $40 = $2040
-    lda #$81
-    sta $07f9
+    
+	// colorize sprites
+	lda #YELLOW
+	sta $d027
+	lda #BLACK
+	sta $d028
+	lda #YELLOW
+	sta $d029
+	lda #YELLOW
+	sta $d02A
+	lda #BLACK
+	sta $d02B
+	lda #BLACK
+	sta $d02C
+	lda #BLACK
+	sta $d02D
+	lda #BLACK
+	sta $d02E
 
-	//sprite2:
-    // $87 * $40
-    lda #$87
-    sta $07fa
-	
-	rts
+    
+	// set sprite pointers
+	lda #$80
+	sta $07F8
+	lda #$81
+	sta $07F9
+	lda #$82
+	sta $07FA
+	lda #$83
+	sta $07FB
+	lda #$84
+	sta $07FC
+	lda #$85
+	sta $07FD
+	lda #$86
+	sta $07FE
+	lda #$87
+	sta $07ff
 
-sprite_colors:
-.byte $01,$05,$07,$01,$01,$01,$01,$01	
+	// positioning sprites
+	lda #$78
+	sta $d000	// #0. sprite X low byte
+	lda #$45
+	sta $d001	// #0. sprite Y
+	lda #$E0
+	sta $d002	// #1. sprite X low byte
+	lda #$39
+	sta $d003	// #1. sprite Y
+	lda #$22
+	sta $d004	// #2. sprite X low byte  sprite Joint
+	lda #$CC
+	sta $d005	// #2. sprite Y  sprite Joint
+	lda #$B9
+	sta $d006	// #3. sprite X low byte
+	lda #$C2
+	sta $d007	// #3. sprite Y
+	lda #$30
+	sta $d008	// #4. sprite X low byte
+	lda #$9A
+	sta $d009	// #4. sprite Y
+	lda #$69
+	sta $d00A	// #5. sprite X low byte
+	lda #$3C
+	sta $d00B	// #5. sprite Y
+    lda #30
+    sta $d00C	// #6. sprite X low byte   sprite Face
+    lda #50
+    sta $d00D	// #6. sprite Y   sprite Face
+	lda #180
+	sta $d00E	// #7. sprite X low byte   sprite Face
+	lda #136
+	sta $d00F	// #7. sprite Y   sprite Face
 
-*=$2000 "sprite 0 skull"
-sprite0:
+	// X coordinate high bits
+	lda #%00010100
+	sta $d010
 
-.byte $00,$7c,$00,$00,$82,$00,$01,$01
-.byte $00,$01,$49,$00,$01,$01,$00,$01
-.byte $02,$00,$00,$b2,$00,$00,$82,$00
-.byte $00,$ba,$00,$00,$aa,$00,$e0,$ba
-.byte $1e,$90,$82,$12,$80,$7c,$02,$58
-.byte $00,$76,$0e,$03,$c0,$03,$9c,$00
-.byte $00,$f0,$00,$0f,$bc,$00,$38,$07
-.byte $80,$20,$00,$f0,$c0,$00,$0e,$01
+	// expand sprites
+	lda #$02
+	sta $d01d
+	lda #$09
+	sta $d017
 
-*=$2040 "sprite 1 tank"
-sprite1:
+	// set multicolor flags
+	lda #$C0
+	sta $d01c
 
-.fill 8, 0
-.byte $00,$40,$00,$00,$70,$00,$00,$1c
-.byte $00,$00,$07,$1f,$c0,$01,$f0,$20
-.byte $00,$60,$10,$00,$40,$10,$00,$40
-.byte $10,$7f,$ff,$fe,$c0,$00,$03,$9f
-.byte $ff,$f9,$30,$00,$05,$25,$55,$54
-.byte $30,$00,$04,$1f,$ff,$f8,$00,$00
-.byte $00,$00,$00,$00,$00,$00,$00,$01
+	// set screen-sprite priority flags
+	lda #$00
+	sta $d01b
 
-*=($87 * $40) "sprite 2 monster"
-sprite2:
+	// turn on sprites
+	lda #%11111111
+	sta $d015
 
-.byte $00,$00,$00,$00,$1f,$80,$01,$ff
-.byte $e0,$0f,$ff,$e0,$1f,$ff,$f0,$3f
-.byte $ff,$f0,$7f,$f9,$f8,$7f,$f9,$f8
-.byte $7f,$ff,$fc,$ff,$ff,$fc,$ff,$ff
-.byte $fe,$c3,$ff,$fe,$81,$ff,$fc,$00
-.byte $ff,$fc,$00,$3f,$f8,$00,$1f,$f8
-.byte $7e,$03,$fc,$3f,$ff,$fe,$1f,$ff
-.byte $fe,$3f,$ff,$ff,$7c,$7e,$0f,$01
