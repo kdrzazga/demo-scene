c95300a4235a5ddc190545234d5f4cf39bf323d9
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-11-20 22:08:05 +0100

    Winter fake demo 1 (#37)

---
+++ temp/linter-fake-demo/sprites-snow.asm
@@ -0,0 +1,12 @@
+handle_snow:
+
+    .var snowYs = List().add(snow1y, snow2y, snow3y, snow4y, snow5y)
+    .for (var i = 0; i <snowYs.size(); i++){
+        inc snowYs.get(i)
+        lda snowYs.get(i)
+        cmp #140
+        bne exit_loop_snow
+        poke snowYs.get(i) : #20
+        exit_loop_snow:
+    }
+    rts
