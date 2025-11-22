862c89dd4e69d242187ba1b712c992a29beb8362
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-11-22 05:33:40 +0100

    extracting ball file

--- commodore64/games/comodopong/pong.asm
+++ commodore64/games/comodopong/pong.asm
@@ -103,46 +103,11 @@ irq1:
     pla
     tax
     pla
-	//jmp move_ball
+	jsr move_ball
 
-move_ball:
-    inc 1024 + 16*40 //TODO: remove
-    lda ball_movement_vertical
-    cmp #DOWN
-    beq move_ball_down
-    jmp move_ball_up
-check_horizontal_move:
-    lda ball_movement_horizontal
-    cmp #LEFT
-    beq move_ball_left
-    jmp move_ball_right
-
-move_ball_down:
-    inc ball_y_address
-    jmp check_horizontal_deflection
-move_ball_up:
-    dec ball_y_address
-    jmp check_horizontal_deflection
-move_ball_left:
-    dec ball_x_address
-    jmp end_movement
-move_ball_right:
-    inc ball_x_address
-    jmp end_movement
-
-check_horizontal_deflection:
-    lda ball_y_address
-    cmp #30
-    beq ceiling_deflection
-    jmp check_horizontal_move
-
-ceiling_deflection:
-    lda #DOWN
-    sta ball_movement_vertical
-    jmp check_horizontal_move
-
-end_movement:
     rti
+
+#import "ball.asm"
 
 counter:
     .byte 0
